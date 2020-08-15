a6011c8d1e2d9d74a882b2f92318c8d4dc7f87c4
jrios002 <32584435+jrios002@users.noreply.github.com>
2020-08-14 19:54:37 -0500

    added firebase database for users and email sign up

--- src/pages/signInAndSignUp/signInAndSignUp.js
+++ src/pages/signInAndSignUp/signInAndSignUp.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../../components/signIn/signIn';
+import SignUp from '../../components/signUp/signUp';
 import './signInAndSignUp.scss';
 
 const SignInAndSignUpPage = () => (
     <div className='signInAndSignUp'>
         <SignIn />
+        <SignUp />
     </div>
 );
 
