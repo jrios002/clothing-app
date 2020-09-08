fb41797a076d21090dd0c265b624dfaf1fbf76d3
jrios002 <32584435+jrios002@users.noreply.github.com>
2020-09-07 21:12:41 -0500

    added remove from cart feature, updated toggle hidden

--- src/components/checkoutItem/checkoutItem.js
+++ src/components/checkoutItem/checkoutItem.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { clearItemFromCart } from '../../redux/cart/cartActions';
 import './checkoutItem.scss';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-    <div className='checkoutItem'>
-        <div className='imageContainer'>
-            <img src={imageUrl} alt='item' />
+const CheckoutItem = ({cartItem, clearItem}) => {
+    const { name, quantity, price, imageUrl } = cartItem;
+    return(
+        <div className='checkoutItem'>
+            <div className='imageContainer'>
+                <img src={imageUrl} alt='item' />
+            </div>
+                <span className='name'>{name}</span>
+                <span className='quantity'>{quantity}</span>
+                <span className='price'>{price}</span>
+            <div className='removeButton' onClick={() => clearItem(cartItem)}>
+                &#10005;
+            </div>
         </div>
-            <span className='name'>{name}</span>
-            <span className='quantity'>{quantity}</span>
-            <span className='price'>{price}</span>
-        <div className='removeButton'>&#10005;</div>
-    </div>
-);
+    );
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
